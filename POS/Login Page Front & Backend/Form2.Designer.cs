cb0cf8c474fdfff1d7423f285cbf0fff9806ec6b
maskman1543 <103424060+maskman1543@users.noreply.github.com>
2024-07-13 16:56:25 +0800

    Database and Global Class file declared for data connection

--- POS/Login Page Front & Backend/Form2.Designer.cs
+++ POS/Login Page Front & Backend/Form2.Designer.cs
@@ -53,9 +53,11 @@
             groupBox1.Controls.Add(textBox1);
             groupBox1.Controls.Add(label1);
             groupBox1.FlatStyle = FlatStyle.Popup;
-            groupBox1.Location = new Point(12, 12);
+            groupBox1.Location = new Point(10, 9);
+            groupBox1.Margin = new Padding(3, 2, 3, 2);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(512, 804);
+            groupBox1.Padding = new Padding(3, 2, 3, 2);
+            groupBox1.Size = new Size(448, 603);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             // 
@@ -67,9 +69,9 @@
             linkLabel1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
             linkLabel1.ForeColor = Color.White;
             linkLabel1.LinkColor = Color.White;
-            linkLabel1.Location = new Point(180, 682);
+            linkLabel1.Location = new Point(158, 512);
             linkLabel1.Name = "linkLabel1";
-            linkLabel1.Size = new Size(146, 25);
+            linkLabel1.Size = new Size(118, 20);
             linkLabel1.TabIndex = 4;
             linkLabel1.TabStop = true;
             linkLabel1.Text = "Forgot Password";
@@ -78,11 +80,12 @@
             // 
             button1.BackColor = Color.Turquoise;
             button1.FlatStyle = FlatStyle.Popup;
-            button1.Font = new Font("Roboto", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
             button1.ForeColor = Color.White;
-            button1.Location = new Point(87, 602);
+            button1.Location = new Point(76, 452);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(331, 46);
+            button1.Size = new Size(290, 34);
             button1.TabIndex = 3;
             button1.Text = "Login";
             button1.UseVisualStyleBackColor = false;
@@ -93,9 +96,10 @@
             checkBox1.BackColor = Color.Transparent;
             checkBox1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
             checkBox1.ForeColor = SystemColors.Control;
-            checkBox1.Location = new Point(87, 542);
+            checkBox1.Location = new Point(76, 406);
+            checkBox1.Margin = new Padding(3, 2, 3, 2);
             checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(193, 32);
+            checkBox1.Size = new Size(154, 25);
             checkBox1.TabIndex = 2;
             checkBox1.Text = "   Show Password";
             checkBox1.UseVisualStyleBackColor = false;
@@ -103,9 +107,10 @@
             // textBox2
             // 
             textBox2.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox2.Location = new Point(87, 466);
+            textBox2.Location = new Point(76, 350);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(331, 43);
+            textBox2.Size = new Size(290, 36);
             textBox2.TabIndex = 1;
             // 
             // label2
@@ -113,20 +118,21 @@
             label2.AutoSize = true;
             label2.BackColor = Color.Transparent;
             label2.FlatStyle = FlatStyle.Flat;
-            label2.Font = new Font("Roboto", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             label2.ForeColor = SystemColors.Control;
-            label2.Location = new Point(87, 420);
+            label2.Location = new Point(76, 315);
             label2.Name = "label2";
-            label2.Size = new Size(124, 30);
+            label2.Size = new Size(100, 24);
             label2.TabIndex = 0;
             label2.Text = "Password";
             // 
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(87, 356);
+            textBox1.Location = new Point(76, 267);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(331, 43);
+            textBox1.Size = new Size(290, 36);
             textBox1.TabIndex = 1;
             // 
             // label1
@@ -134,21 +140,22 @@
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
             label1.FlatStyle = FlatStyle.Flat;
-            label1.Font = new Font("Roboto", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = SystemColors.Control;
-            label1.Location = new Point(87, 310);
+            label1.Location = new Point(76, 232);
             label1.Name = "label1";
-            label1.Size = new Size(126, 30);
+            label1.Size = new Size(105, 24);
             label1.TabIndex = 0;
             label1.Text = "Username";
             // 
             // Form2
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(536, 828);
+            ClientSize = new Size(469, 621);
             Controls.Add(groupBox1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
             MaximizeBox = false;
             Name = "Form2";
             StartPosition = FormStartPosition.CenterScreen;
--- Registration/acctRegistration_form.cs
+++ Registration/acctRegistration_form.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace Service_Management_System.Registration
 {
+
+
     public partial class acctRegistration_form : Form
     {
         public acctRegistration_form()
         {
             InitializeComponent();
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txb_Email_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
