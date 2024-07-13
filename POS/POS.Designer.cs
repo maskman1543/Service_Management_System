1211d49b4f44a3bc78e073e43ae71c6abc3a3260
Gil Bernard F. Maglinte <127652117+Berot1@users.noreply.github.com>
2024-07-13 20:49:47 +0800

    POS

--- POS/POS.Designer.cs
+++ POS/POS.Designer.cs
@@ -28,17 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(POSForm));
             panel1 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(3, 83, 115);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1440, 72);
+            panel1.Size = new Size(1440, 78);
             panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.ImageAlign = ContentAlignment.TopCenter;
+            button1.Location = new Point(12, 8);
+            button1.Name = "button1";
+            button1.Size = new Size(83, 63);
+            button1.TabIndex = 1;
+            button1.Text = "Search";
+            button1.TextAlign = ContentAlignment.BottomCenter;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 8F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.White;
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.ImageAlign = ContentAlignment.TopCenter;
+            button2.Location = new Point(101, 8);
+            button2.Name = "button2";
+            button2.Size = new Size(83, 63);
+            button2.TabIndex = 2;
+            button2.Text = "Customer";
+            button2.TextAlign = ContentAlignment.BottomCenter;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = Color.White;
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.ImageAlign = ContentAlignment.TopCenter;
+            button3.Location = new Point(190, 9);
+            button3.Name = "button3";
+            button3.Size = new Size(83, 63);
+            button3.TabIndex = 3;
+            button3.Text = "Search";
+            button3.TextAlign = ContentAlignment.BottomCenter;
+            button3.UseVisualStyleBackColor = true;
             // 
             // POSForm
             // 
@@ -51,11 +104,15 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "POS";
             Load += POS_Load;
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel panel1;
+        private Button button1;
+        private Button button3;
+        private Button button2;
     }
 }
