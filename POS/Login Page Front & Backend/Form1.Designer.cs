ada16f01a7501aed123f36bcec9556caeef6e85c
JpCoder <Aserpirefiver@gmail.com>
2024-07-13 15:44:43 +0800

    Login Page Revised push v3

---
+++ POS/Login Page Front & Backend/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Service_Management_System.POS.Login_Page_Front_and_Back_End
+{
+    partial class Splash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Timer to control splash screen duration.
+        /// </summary>
+        private System.Windows.Forms.Timer timer;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splash));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 5000;
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Splash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = (System.Drawing.Image)resources.GetObject("$this.BackgroundImage");
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(683, 503);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Splash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Splash";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ POS/Login Page Front & Backend/Form1.cs
@@ -0,0 +1,27 @@
+﻿using Service_Management_System.POS.Login_Page_Front___Backend;
+using System;
+using System.Windows.Forms; // Ensure this using directive is present for Windows Forms Timer
+
+namespace Service_Management_System.POS.Login_Page_Front_and_Back_End
+{
+    public partial class Splash : Form
+    {
+        public Splash()
+        {
+            InitializeComponent();
+
+            // Configure the timer
+            timer.Interval = 5000; // 5 seconds
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop(); // Stop the timer to avoid it firing again
+            Form2 mainForm = new Form2();
+            mainForm.Show(); // Show the main form
+            this.Hide(); // Hide the splash screen
+        }
+    }
+}
