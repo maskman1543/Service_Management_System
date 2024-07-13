c1898a2d197f6a58c6be60221f6504850993bae8
jaspher siloy <jaspher siloy@LAPTOP-GMR53LPQ>
2024-07-13 15:14:28 +0800

    registration

---
+++ Registration/acctRegistration_form.Designer.cs
@@ -0,0 +1,415 @@
+﻿namespace Service_Management_System.Registration
+{
+    partial class acctRegistration_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(acctRegistration_form));
+            panel1 = new Panel();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            txbLname = new TextBox();
+            txbFname = new TextBox();
+            label6 = new Label();
+            txb_phoneNumber = new TextBox();
+            txb_Email = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            textBox1 = new TextBox();
+            label9 = new Label();
+            textBox2 = new TextBox();
+            label10 = new Label();
+            textBox3 = new TextBox();
+            label11 = new Label();
+            button2 = new Button();
+            label12 = new Label();
+            checkBox1 = new CheckBox();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            label13 = new Label();
+            label14 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(3, 83, 115);
+            panel1.Controls.Add(label14);
+            panel1.Controls.Add(label13);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(button1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(378, 730);
+            panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Tahoma", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(47, 598);
+            button1.Name = "button1";
+            button1.Size = new Size(263, 49);
+            button1.TabIndex = 0;
+            button1.Text = "SIGN IN";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(112, 571);
+            label1.Name = "label1";
+            label1.Size = new Size(134, 24);
+            label1.TabIndex = 1;
+            label1.Text = "SIGN IN HERE";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Trebuchet MS", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.FromArgb(3, 83, 115);
+            label2.Location = new Point(461, 60);
+            label2.Name = "label2";
+            label2.Size = new Size(392, 43);
+            label2.TabIndex = 2;
+            label2.Text = "ACCOUNT REGISTRATION";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Tahoma", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(403, 157);
+            label3.Name = "label3";
+            label3.Size = new Size(145, 28);
+            label3.TabIndex = 3;
+            label3.Text = "Personal Info";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = Color.DimGray;
+            label4.Location = new Point(426, 254);
+            label4.Name = "label4";
+            label4.Size = new Size(89, 21);
+            label4.TabIndex = 4;
+            label4.Text = "Last Name";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.ForeColor = Color.DimGray;
+            label5.Location = new Point(426, 215);
+            label5.Name = "label5";
+            label5.Size = new Size(91, 21);
+            label5.TabIndex = 5;
+            label5.Text = "First Name";
+            // 
+            // txbLname
+            // 
+            txbLname.Cursor = Cursors.IBeam;
+            txbLname.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txbLname.Location = new Point(523, 248);
+            txbLname.Name = "txbLname";
+            txbLname.Size = new Size(290, 30);
+            txbLname.TabIndex = 6;
+            // 
+            // txbFname
+            // 
+            txbFname.Cursor = Cursors.IBeam;
+            txbFname.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txbFname.Location = new Point(523, 209);
+            txbFname.Name = "txbFname";
+            txbFname.Size = new Size(290, 30);
+            txbFname.TabIndex = 7;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Tahoma", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(403, 325);
+            label6.Name = "label6";
+            label6.Size = new Size(165, 28);
+            label6.TabIndex = 8;
+            label6.Text = "Contact Details";
+            // 
+            // txb_phoneNumber
+            // 
+            txb_phoneNumber.Cursor = Cursors.IBeam;
+            txb_phoneNumber.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txb_phoneNumber.Location = new Point(670, 402);
+            txb_phoneNumber.Name = "txb_phoneNumber";
+            txb_phoneNumber.Size = new Size(235, 30);
+            txb_phoneNumber.TabIndex = 12;
+            // 
+            // txb_Email
+            // 
+            txb_Email.Cursor = Cursors.IBeam;
+            txb_Email.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            txb_Email.Location = new Point(403, 402);
+            txb_Email.Name = "txb_Email";
+            txb_Email.Size = new Size(251, 30);
+            txb_Email.TabIndex = 11;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.ForeColor = Color.DimGray;
+            label7.Location = new Point(735, 378);
+            label7.Name = "label7";
+            label7.Size = new Size(118, 21);
+            label7.TabIndex = 10;
+            label7.Text = "Phone Number";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.ForeColor = Color.DimGray;
+            label8.Location = new Point(493, 378);
+            label8.Name = "label8";
+            label8.Size = new Size(51, 21);
+            label8.TabIndex = 9;
+            label8.Text = "Email";
+            // 
+            // textBox1
+            // 
+            textBox1.Cursor = Cursors.IBeam;
+            textBox1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(25, 57);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(251, 30);
+            textBox1.TabIndex = 14;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.ForeColor = Color.DimGray;
+            label9.Location = new Point(29, 33);
+            label9.Name = "label9";
+            label9.Size = new Size(85, 21);
+            label9.TabIndex = 13;
+            label9.Text = "Username";
+            // 
+            // textBox2
+            // 
+            textBox2.Cursor = Cursors.IBeam;
+            textBox2.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(292, 57);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(251, 30);
+            textBox2.TabIndex = 16;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.ForeColor = Color.DimGray;
+            label10.Location = new Point(296, 33);
+            label10.Name = "label10";
+            label10.Size = new Size(81, 21);
+            label10.TabIndex = 15;
+            label10.Text = "Password";
+            // 
+            // textBox3
+            // 
+            textBox3.Cursor = Cursors.IBeam;
+            textBox3.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(292, 122);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(251, 30);
+            textBox3.TabIndex = 18;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label11.ForeColor = Color.DimGray;
+            label11.Location = new Point(296, 98);
+            label11.Name = "label11";
+            label11.Size = new Size(143, 21);
+            label11.TabIndex = 17;
+            label11.Text = "Confirm Password";
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(3, 83, 115);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Tahoma", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(25, 119);
+            button2.Name = "button2";
+            button2.Size = new Size(241, 38);
+            button2.TabIndex = 2;
+            button2.Text = "SIGN UP";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label12.ForeColor = Color.Black;
+            label12.Location = new Point(912, 0);
+            label12.Name = "label12";
+            label12.Size = new Size(22, 24);
+            label12.TabIndex = 2;
+            label12.Text = "X";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(411, 158);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(132, 24);
+            checkBox1.TabIndex = 19;
+            checkBox1.Text = "Show Password";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(73, 29);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(207, 175);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(checkBox1);
+            panel2.Controls.Add(label9);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(label10);
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(label11);
+            panel2.Dock = DockStyle.Bottom;
+            panel2.Location = new Point(378, 476);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(556, 254);
+            panel2.TabIndex = 3;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Brush Script MT", 48F, FontStyle.Italic, GraphicsUnit.Point);
+            label13.ForeColor = Color.White;
+            label13.Location = new Point(0, 189);
+            label13.Name = "label13";
+            label13.Size = new Size(379, 98);
+            label13.TabIndex = 3;
+            label13.Text = "Service Flow";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Microsoft YaHei", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label14.ForeColor = Color.Gainsboro;
+            label14.Location = new Point(47, 275);
+            label14.Name = "label14";
+            label14.Size = new Size(288, 27);
+            label14.TabIndex = 4;
+            label14.Text = "Service Management System";
+            // 
+            // acctRegistration_form
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(934, 730);
+            Controls.Add(panel2);
+            Controls.Add(label12);
+            Controls.Add(txb_phoneNumber);
+            Controls.Add(txb_Email);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label6);
+            Controls.Add(txbFname);
+            Controls.Add(txbLname);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "acctRegistration_form";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "acctRegistration_form";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox txbLname;
+        private TextBox txbFname;
+        private Label label6;
+        private TextBox txb_phoneNumber;
+        private TextBox txb_Email;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox1;
+        private Label label9;
+        private TextBox textBox2;
+        private Label label10;
+        private TextBox textBox3;
+        private Label label11;
+        private Button button2;
+        private Label label12;
+        private CheckBox checkBox1;
+        private PictureBox pictureBox1;
+        private Panel panel2;
+        private Label label14;
+        private Label label13;
+    }
+}
