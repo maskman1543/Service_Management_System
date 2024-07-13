1d0f1f6f4cbc85c91a22c0bfda300f6f0c274b43
Gil Bernard F. Maglinte <127652117+Berot1@users.noreply.github.com>
2024-07-13 11:19:58 +0800

    pos

---
+++ POS/POS.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Service_Management_System.POS
+{
+    partial class POS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // POS
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1440, 1024);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "POS";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "POS";
+            Load += POS_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ POS/POS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Service_Management_System.POS
+{
+    public partial class POS : Form
+    {
+        public POS()
+        {
+            InitializeComponent();
+        }
+
+        private void POS_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
