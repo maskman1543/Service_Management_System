e7d744a32e36a27fba509d20002ad9437d414eb8
jaspher siloy <jaspher siloy@LAPTOP-GMR53LPQ>
2024-07-19 14:28:12 +0800

    UI products and services

--- DASHBOARD/ProductsServicesForm.cs
+++ DASHBOARD/ProductsServicesForm.cs
@@ -8,6 +8,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Forms;
 
 namespace Service_Management_System.DASHBOARD
@@ -15,6 +16,8 @@ namespace Service_Management_System.DASHBOARD
     public partial class ProductsServicesForm : Form
     {
         POSForm POSForm = new POSForm();
+        bool sidebarExpand;
+        private Form currentChildForm;
 
         public ProductsServicesForm()
         {
@@ -68,6 +71,85 @@ namespace Service_Management_System.DASHBOARD
             catch (Exception ex)
             {
                 MessageBox.Show("An Error occurred: " + ex.Message);
+            }
+        }
+
+        private void sidebar_addproducts_Tick(object sender, EventArgs e)
+        {
+            if (sidebarExpand)
+            {
+                panel3.Width -= 10;
+                if (panel3.Width == panel3.MinimumSize.Width)
+                {
+                    sidebarExpand = false;
+                    sidebar_addproducts.Stop();
+                }
+            }
+            else
+            {
+                panel3.Width += 10;
+                if (panel3.Width == panel3.MaximumSize.Width)
+                {
+                    sidebarExpand = true;
+                    sidebar_addproducts.Stop();
+                }
+            }
+        }
+        private void OpenchildForm(Form childForm)
+        {
+            if (currentChildForm != null)
+            {
+                currentChildForm.Close();
+            }
+            currentChildForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelpro_service.Controls.Add(childForm);
+            panelpro_service.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            sidebar_addproducts.Start();
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            sidebar_addproducts.Start();
+        }
+
+        private void btnDetails_Click(object sender, EventArgs e)
+        {
+            OpenchildForm(new addproduct_form());
+        }
+
+        private void btnStockControl_Click(object sender, EventArgs e)
+        {
+            OpenchildForm(new stockControl_form());
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form is stockControl_form)
+                {
+                    form.Hide(); // Hide the AnalysisForm if it is open
+                    return; // Exit the method
+                }
+                else if (form is addproduct_form)
+                {
+                    form.Hide(); // Hide the AnalysisForm if it is open
+                    return; // Exit the method
+                }
             }
         }
     }
