5581ca9b2fcbf81008ad60c9b9fc6e49555d8bed
shiela quevedo <shielaquevedo@yahoo.com>
2024-07-20 16:34:36 +0800

    FIXED JOBORDER FORM AND COMPLTE FUNCTIONALITY

--- POS/jonOrder_form.cs
+++ POS/jonOrder_form.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.OleDb;
 using System.Windows.Forms;
 
 namespace Service_Management_System.POS
@@ -15,6 +9,8 @@ namespace Service_Management_System.POS
         public jonOrder_form()
         {
             InitializeComponent();
+            btnContinue.Click -= new EventHandler(btnContinue_Click); // Detach any existing handlers
+            btnContinue.Click += new EventHandler(btnContinue_Click); // Attach the handler
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -24,42 +20,126 @@ namespace Service_Management_System.POS
 
         private void FirstNameValue_Enter(object sender, EventArgs e)
         {
-            FirstNameValue.Text = "";
+            if (FirstNameValue.Text == "Customer name")
+            {
+                FirstNameValue.Text = "";
+            }
         }
 
         private void FirstNameValue_Leave(object sender, EventArgs e)
         {
-            FirstNameValue.Text = "Customer name";
+            if (string.IsNullOrWhiteSpace(FirstNameValue.Text))
+            {
+                FirstNameValue.Text = "Customer name";
+            }
         }
 
         private void textBox1_Enter(object sender, EventArgs e)
         {
-            textBox1.Text = "";
+            if (textBox1.Text == "Phone no.")
+            {
+                textBox1.Text = "";
+            }
         }
 
         private void textBox1_Leave(object sender, EventArgs e)
         {
-            textBox1.Text = "Phone no.";
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                textBox1.Text = "Phone no.";
+            }
         }
 
         private void textBox2_Enter(object sender, EventArgs e)
         {
-            textBox2.Text = "";
+            if (textBox2.Text == "Vehicle type")
+            {
+                textBox2.Text = "";
+            }
         }
 
         private void textBox2_Leave(object sender, EventArgs e)
         {
-            textBox2.Text = "Vehicle type";
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                textBox2.Text = "Vehicle type";
+            }
         }
 
         private void textBox3_Enter(object sender, EventArgs e)
         {
-            textBox3.Text = "";
+            if (textBox3.Text == "Plate no.")
+            {
+                textBox3.Text = "";
+            }
         }
 
         private void textBox3_Leave(object sender, EventArgs e)
         {
-            textBox3.Text = "Plate no.";
+            if (string.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                textBox3.Text = "Plate no.";
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnContinue_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(FirstNameValue.Text) || FirstNameValue.Text == "Customer name" ||
+                string.IsNullOrWhiteSpace(textBox1.Text) || textBox1.Text == "Phone no." ||
+                string.IsNullOrWhiteSpace(textBox2.Text) || textBox2.Text == "Vehicle type" ||
+                string.IsNullOrWhiteSpace(textBox3.Text) || textBox3.Text == "Plate no.")
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return;
+            }
+
+            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Elementz\source\repos\Service_Management_System\bin\Debug\net6.0-windows\ServiceManagementSYstem.accdb";
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                OleDbTransaction transaction = connection.BeginTransaction();
+
+                try
+                {
+                    string insertVehicleQuery = "INSERT INTO VehicleTb (Model, PlateNumber) VALUES (@Model, @PlateNumber)";
+                    int vehicleID;
+                    using (OleDbCommand cmdVehicle = new OleDbCommand(insertVehicleQuery, connection, transaction))
+                    {
+                        cmdVehicle.Parameters.AddWithValue("@Model", textBox2.Text);
+                        cmdVehicle.Parameters.AddWithValue("@PlateNumber", textBox3.Text);
+                        cmdVehicle.ExecuteNonQuery();
+
+                        string getVehicleIDQuery = "SELECT @@IDENTITY";
+                        using (OleDbCommand cmdGetVehicleID = new OleDbCommand(getVehicleIDQuery, connection, transaction))
+                        {
+                            vehicleID = Convert.ToInt32(cmdGetVehicleID.ExecuteScalar());
+                        }
+                    }
+
+                    string insertCustomerQuery = "INSERT INTO CustomerTb (FirstName, Contact, VehicleID) VALUES (@FirstName, @Contact, @VehicleID)";
+                    using (OleDbCommand cmdCustomer = new OleDbCommand(insertCustomerQuery, connection, transaction))
+                    {
+                        cmdCustomer.Parameters.AddWithValue("@FirstName", FirstNameValue.Text);
+                        cmdCustomer.Parameters.AddWithValue("@Contact", textBox1.Text);
+                        cmdCustomer.Parameters.AddWithValue("@VehicleID", vehicleID);
+                        cmdCustomer.ExecuteNonQuery();
+                    }
+
+                    transaction.Commit();
+                    MessageBox.Show("Data inserted successfully!");
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    MessageBox.Show("An error occurred: " + ex.Message);
+                }
+            }
         }
     }
 }
