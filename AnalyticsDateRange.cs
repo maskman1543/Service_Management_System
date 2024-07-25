0593350b5469a1e4ee8d74e1aa6855676c4a17fc
jaspher siloy <jaspher siloy@LAPTOP-GMR53LPQ>
2024-07-25 15:58:25 +0800

    analytics datarange selection

---
+++ AnalyticsDateRange.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service_Management_System
+{
+    internal class AnalyticsDateRange
+    {
+        public static DateTime DateStart { get; set; } = DateTime.Now.AddDays(-7);
+        public static DateTime DateEnd { get; set; } = DateTime.Now;
+    }
+}
